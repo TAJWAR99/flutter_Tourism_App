07117440400e2a269d0f2db704f8fbd2787e7c0f
skatik <skatiktajwar14@gmail.com>
2021-08-22 13:43:03 +0600

    updateapp

--- travel_app/lib/main.dart
+++ travel_app/lib/main.dart
@@ -1,7 +1,8 @@
-import 'dart:html';
+// import 'dart:html';
 import 'package:flutter/cupertino.dart';
 //import 'package:material_cupertino_search_bar/material_cupertino_search_bar.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Description(),
+      // home: MyHomePage(title: 'Travel App'),
     );
   }
 }
@@ -36,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
+          title: Center(child: Text(widget.title)),
         ),
         body: Column(
           children: <Widget>[
@@ -51,9 +53,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 160,
                       child: Row(
                         children: [
-                          Icon(
-                            Icons.face,
-                            size: 40,
+                          Image(
+                            height: 40,
+                            width: 45,
+                            image: AssetImage("images/prodp.jpg"),
                           ),
                           Text(
                             "Welcome! Atik",
@@ -81,6 +84,9 @@ class _MyHomePageState extends State<MyHomePage> {
               itemSize: 30,
               backgroundColor: Colors.white70,
             ),
+            SizedBox(
+              height: 20,
+            ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Row(
@@ -96,8 +102,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
             Container(
-              height: 320,
+              height: 300,
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: [
@@ -124,11 +133,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
+            SizedBox(
+              height: 30,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: Text(
                     'Top Destination',
                     style: TextStyle(
@@ -139,6 +151,67 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
+            Container(
+              height: 221,
+              alignment: Alignment.center,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    topDest(
+                      text1: "Dubrovnik",
+                      text2: "Croatia",
+                      image: 'Dubrovnik_Croatia.jpg',
+                    ),
+                    topDest(
+                      text1: "Giresun",
+                      text2: "Turkey",
+                      image: 'Giresun_Turkey.jpg',
+                    ),
+                    topDest(
+                      text1: "Hallstatt",
+                      text2: "Österreich",
+                      image: 'Hallstatt_Österreich.jpg',
+                    ),
+                    topDest(
+                      text1: "Miami FL",
+                      text2: "USA",
+                      image: 'Miami_FL_USA.jpg',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Container(
+            //   alignment: Alignment.center,
+            //   height: 290,
+            //   color: Colors.pink,
+            //   child: ListView(
+            //     scrollDirection: Axis.horizontal,
+            //     children: [
+            //       topDest(
+            //         text1: "Dubrovnik",
+            //         text2: "Croatia",
+            //         image: 'Dubrovnik_Croatia.jpg',
+            //       ),
+            //       topDest(
+            //         text1: "Giresun",
+            //         text2: "Turkey",
+            //         image: 'Giresun_Turkey.jpg',
+            //       ),
+            //       topDest(
+            //         text1: "Hallstatt",
+            //         text2: "Österreich",
+            //         image: 'Hallstatt_Österreich.jpg',
+            //       ),
+            //       topDest(
+            //         text1: "Miami FL",
+            //         text2: "USA",
+            //         image: 'Miami_FL_USA.jpg',
+            //       ),
+            //     ],
+            //   ),
+            // ),
           ],
         ),
         //Bottom Navigation Bar
@@ -183,6 +256,83 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class topDest extends StatelessWidget {
+  final String text1;
+  final String text2;
+  final String image;
+
+  const topDest({
+    Key? key,
+    required this.text1,
+    required this.text2,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 100,
+        width: 250,
+        // color: Colors.red,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image(
+                fit: BoxFit.cover,
+                width: 150,
+                image: AssetImage('images/' + image),
+              ),
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    // alignment: Alignment.center,
+                    height: 50,
+                    width: 100,
+                    //color: Colors.pink,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "$text1",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        // SizedBox(
+                        //   height: 5,
+                        // )
+                        Text(
+                          "$text2",
+                          style: TextStyle(
+                            //fontSize: 18,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class container extends StatelessWidget {
   final String image;
   final int price;
@@ -199,7 +349,7 @@ class container extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Container(
-        height: 320,
+        height: 300,
         width: 270,
         decoration: BoxDecoration(
           color: Colors.red,
@@ -216,7 +366,7 @@ class container extends StatelessWidget {
                   child: Image(
                     fit: BoxFit.cover,
                     image: AssetImage("images/" + image),
-                    height: 296,
+                    height: 276,
                     width: 270,
                   ),
                 ),
@@ -285,6 +435,150 @@ class container extends StatelessWidget {
       //     ),
       //   ),
       // ),
+    );
+  }
+}
+
+class Description extends StatelessWidget {
+  const Description({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Center(child: Text("Hello")),
+      // ),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 400,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(50),
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "About",
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      height: 100,
+                      width: 400,
+                      child: Text(
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: Text(
+                      "Gallery",
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            height: 144,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                container2(image: 'Dubrovnik_Croatia.jpg'),
+                container2(image: 'Giresun_Turkey.jpg'),
+                container2(image: 'Hallstatt_Österreich.jpg'),
+                container2(image: 'Miami_FL_USA.jpg'),
+                container2(image: 'Venise_Italie.jpg'),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          SizedBox(
+            height: 80,
+            width: 450,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_forward),
+              label: Text(
+                "Book Now",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class container2 extends StatelessWidget {
+  final String image;
+
+  const container2({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        height: 120,
+        width: 140,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Stack(
+              //alignment: Alignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image(
+                    fit: BoxFit.cover,
+                    image: AssetImage("images/" + image),
+                    height: 120,
+                    width: 140,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
